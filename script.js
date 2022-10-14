63ea5d31113f0db95a0899ed5d79a3a1ce9f1b23
Lunamzelo <lunamzelo@gmail.com>
2022-10-15 00:01:02 +0100

    updated script.js

--- script.js
+++ script.js
@@ -1,12 +1,90 @@
-//You can edit ALL of the code here
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+    const episodes = getAllEpisodes();
+    makePageForEpisodes(episodes);
+    document.getElementById("searchInput").addEventListener("input", makePageForMatchingEpisodes);
 }
 
+
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+    const container = document.getElementById("episodes");
+    episodeList.forEach((episode) => {
+       const card = document.createElement("div");
+       card.className = "card";
+         const title = document.createElement("h1");
+            title.className = "title";
+            title.innerText = `${episode.name} - S${episode.season
+                .toString()
+                .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+            card.appendChild(title);
+            const image = document.createElement("img");
+            image.className = "image";
+            image.src = episode.image.medium;
+            card.appendChild(image);
+            const summary = document.createElement("p");
+            summary.className = "summary";
+            summary.innerHTML = episode.summary;
+            card.appendChild(summary);
+            container.appendChild(card);
+
+
+
+
+
+    });
+    const count = document.getElementById("countDisplay");
+    count.innerText = `Displaying ${episodeList.length}/${getAllEpisodes().length} episodes`;
+    select(episodeList);
+}
+function makePageForMatchingEpisodes() {
+    const searchInput = document.getElementById("searchInput").value;
+    const episodes = getAllEpisodes();
+    const matchingEpisodes = episodes.filter((episode) => {
+        return (
+            episode.name.toLowerCase().includes(searchInput.toLowerCase())||
+            episode.summary.toLowerCase().includes(searchInput.toLowerCase())
+        );
+    });
+    const container = document.getElementById("episodes");
+    container.innerHTML = "";
+    makePageForEpisodes(matchingEpisodes);
+}
+function select(episodes) {
+  const shows= document.getElementById("selectShow");
+    shows.innerHTML = "";
+
+    episodes.forEach((show) => {
+        const option = document.createElement("option");
+        option.innerText = show.name;
+        option.value = show.id;
+        shows.appendChild(option);
+    }
+    );
+
 }
 
 window.onload = setup;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
